40d6885c37b4521bac9c5c6ca46ec5fe896108fe
un0528 <84552395+un0528@users.noreply.github.com>
2026-03-16 11:29:30 +0800

    Remove unused code

--- app/framework/admin-app/packages/api/scripts/codegen.ts
+++
@@ -1,9 +0,0 @@
-import { generateApiClient, watchApiClient } from '@ai-partner-x/aiko-boot-codegen';
-
-const isWatch = process.argv.includes('--watch') || process.argv.includes('-w');
-
-if (isWatch) {
-  watchApiClient();
-} else {
-  generateApiClient();
-}
